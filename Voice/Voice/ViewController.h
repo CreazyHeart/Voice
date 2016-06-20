7d58a38b555f2fdf4b4d2f81d424959d612e1ec5
shanxinhu <shanxinhu@sina.com>
2016-06-20 11:09:34 +0800

    [comment] : 初始化项目

---
+++ Voice/Voice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Voice
+//
+//  Created by 户善新 on 16/6/20.
+//  Copyright © 2016年 户善新. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
